104d383d82cf1228f2b1f5002d1f003ecdd77d09
agoral.sky320 <agoral.sky320@gmail.com>
2025-08-09 18:08:50 +0300

    init

---
+++ cmd/zmux-server/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Create a new Gin router
+	r := gin.Default()
+
+	// Simple GET endpoint
+	r.GET("/api/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	// Start server
+	r.Run(":8080") // Default listens on :8080
+}
