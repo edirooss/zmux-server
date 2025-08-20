9f6b982b7e4d8055376d27159d935401b03c61bf
edirooss <agoral.sky320@gmail.com>
2025-08-20 19:33:10 +0300

    pretified create structs; seperated input,output types

--- pkg/models/channelmodel/create_zmux_channel_req.go
+++ pkg/models/channelmodel/create_zmux_channel_req.go
@@ -12,33 +12,37 @@ import "github.com/mcuadros/go-defaults"
 //	if err := c.ShouldBindJSON(&req); err != nil { ... } // provided fields override defaults
 //	// req.Name and req.Input.InputURL must be set by the client.
 type CreateZmuxChannelReq struct {
-	Name string `json:"name" binding:"required,min=1,max=100"` // must be provided by client
+	Name string `json:"name"                       binding:"required,min=1,max=100"` // must be provided by client
 
 	// --- Remux configuration ---
-	Input struct {
-		URL             string `json:"url"       binding:"required"` // must be provided by client
-		AVIOFlags       string `json:"avioflags"       default:""`
-		ProbeSize       uint   `json:"probesize"       default:"5000000"`
-		AnalyzeDuration uint   `json:"analyzeduration" default:"0"`
-		FFlags          string `json:"fflags"          default:""`
-		MaxDelay        int    `json:"max_delay"       default:"-1"`
-		LocalAddr       string `json:"localaddr"       default:""`
-		Timeout         uint   `json:"timeout"         default:"3000000"`
-		RTSPTransport   string `json:"rtsp_transport"  default:""`
-	} `json:"input"`
-	Output struct {
-		URL       string `json:"url" default:"/dev/null"`
-		LocalAddr string `json:"localaddr"  default:""`
-		PktSize   uint   `json:"pkt_size"   default:"1316"`
-		MapVideo  bool   `json:"map_video" default:"true"`
-		MapAudio  bool   `json:"map_audio" default:"true"`
-		MapData   bool   `json:"map_data"  default:"true"`
-	} `json:"output"`
+	Input  CreateInput  `json:"input"`
+	Output CreateOutput `json:"output"`
 	// ----------------------------
 
 	// Systemd settings
-	Enabled    bool `json:"enabled"    default:"true"`
-	RestartSec uint `json:"restart_sec" default:"3"`
+	Enabled    bool `json:"enabled"                default:"true"`
+	RestartSec uint `json:"restart_sec"            default:"3"`
+}
+
+type CreateInput struct {
+	URL             string `json:"url"             binding:"required"` // must be provided by client
+	AVIOFlags       string `json:"avioflags"       default:""`
+	ProbeSize       uint   `json:"probesize"       default:"5000000"`
+	AnalyzeDuration uint   `json:"analyzeduration" default:"0"`
+	FFlags          string `json:"fflags"          default:""`
+	MaxDelay        int    `json:"max_delay"       default:"-1"`
+	LocalAddr       string `json:"localaddr"       default:""`
+	Timeout         uint   `json:"timeout"         default:"3000000"`
+	RTSPTransport   string `json:"rtsp_transport"  default:""`
+}
+
+type CreateOutput struct {
+	URL       string `json:"url"                   default:"/dev/null"`
+	LocalAddr string `json:"localaddr"             default:""`
+	PktSize   uint   `json:"pkt_size"              default:"1316"`
+	MapVideo  bool   `json:"map_video"             default:"true"`
+	MapAudio  bool   `json:"map_audio"             default:"true"`
+	MapData   bool   `json:"map_data"              default:"true"`
 }
 
 // NewCreateZmuxChannelReq returns a struct pre-filled with defaults.
