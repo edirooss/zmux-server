1cd2cc8d8fa4349892af0213ffc11cdd700362f0
edirooss <agoral.sky320@gmail.com>
2025-08-24 20:20:25 +0300

    refactor(channel): extract input/output structs from ZmuxChannel

--- internal/domain/channel/model.go
+++ internal/domain/channel/model.go
@@ -6,34 +6,33 @@ import (
 )
 
 type ZmuxChannel struct {
-	ID   int64   `json:"id"`   //
-	Name *string `json:"name"` // nullable
+	ID         int64             `json:"id"`          //
+	Name       *string           `json:"name"`        // nullable
+	Input      ZmuxChannelInput  `json:"input"`       //
+	Output     ZmuxChannelOutput `json:"output"`      //
+	Enabled    bool              `json:"enabled"`     // (on enabled=true, requires both input.url and name to be non-null)
+	RestartSec uint              `json:"restart_sec"` //
+}
 
-	// --- Remux configuration ---
-	Input struct {
-		URL             *string `json:"url"`             // nullable (on non-null, requires name to be non-null)
-		AVIOFlags       *string `json:"avioflags"`       // nullable
-		Probesize       uint    `json:"probesize"`       //
-		Analyzeduration uint    `json:"analyzeduration"` //
-		FFlags          *string `json:"fflags"`          // nullable
-		MaxDelay        int     `json:"max_delay"`       //
-		Localaddr       *string `json:"localaddr"`       // nullable
-		Timeout         uint    `json:"timeout"`         //
-		RTSPTransport   *string `json:"rtsp_transport"`  // nullable
-	} `json:"input"`
-	Output struct {
-		URL       *string `json:"url"`       // nullable
-		Localaddr *string `json:"localaddr"` // nullable
-		PktSize   uint    `json:"pkt_size"`  //
-		MapVideo  bool    `json:"map_video"` //
-		MapAudio  bool    `json:"map_audio"` //
-		MapData   bool    `json:"map_data"`  //
-	} `json:"output"`
-	// ----------------------------
+type ZmuxChannelInput struct {
+	URL             *string `json:"url"`             // nullable (on non-null, requires name to be non-null)
+	AVIOFlags       *string `json:"avioflags"`       // nullable
+	Probesize       uint    `json:"probesize"`       //
+	Analyzeduration uint    `json:"analyzeduration"` //
+	FFlags          *string `json:"fflags"`          // nullable
+	MaxDelay        int     `json:"max_delay"`       //
+	Localaddr       *string `json:"localaddr"`       // nullable
+	Timeout         uint    `json:"timeout"`         //
+	RTSPTransport   *string `json:"rtsp_transport"`  // nullable
+}
 
-	// Systemd settings
-	Enabled    bool `json:"enabled"`     // (on enabled=true, requires both input.url and name to be non-null)
-	RestartSec uint `json:"restart_sec"` //
+type ZmuxChannelOutput struct {
+	URL       *string `json:"url"`       // nullable
+	Localaddr *string `json:"localaddr"` // nullable
+	PktSize   uint    `json:"pkt_size"`  //
+	MapVideo  bool    `json:"map_video"` //
+	MapAudio  bool    `json:"map_audio"` //
+	MapData   bool    `json:"map_data"`  //
 }
 
 func (ch *ZmuxChannel) Validate() error {
